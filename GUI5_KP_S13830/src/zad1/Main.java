8461be69221b8878a411d10c4fa9481b24d5c374
Piotrekkot <piotr.kotowski96@gmail.com>
2017-08-19 15:40:49 +0200

    Add files via upload

---
+++ GUI5_KP_S13830/src/zad1/Main.java
@@ -0,0 +1,52 @@
+/**
+ *
+ *  @author Kotowski Piotr S13830
+ *
+ */
+
+package zad1;
+
+
+public class Main {
+  public static void main(String[] args)
+  {
+    Spiewak s1 = new Spiewak("Carrey"){
+
+		@Override
+		String spiewaj() {
+			// TODO Auto-generated method stub
+			return "oooooooooooo";
+		}
+
+      
+    };
+
+    Spiewak s2 = new Spiewak("Houston"){
+
+		@Override
+		String spiewaj() {
+			// TODO Auto-generated method stub
+			return "a4iBBiii";
+		}
+      
+    };
+
+    Spiewak s3 = new Spiewak("Madonna"){
+
+		@Override
+		String spiewaj() {
+			// TODO Auto-generated method stub
+			return "aAa";
+		}
+      
+    };
+
+    Spiewak sp[] = {s1, s2, s3};
+
+    for (Spiewak s : sp)
+      System.out.println(s);
+
+
+    System.out.println("\n" + Spiewak.najglosniej(sp));
+  }
+}
---
+++ GUI5_KP_S13830/src/zad1/Spiewak.java
@@ -0,0 +1,27 @@
+/**
+ *
+ *  @author Kotowski Piotr S13830
+ *
+ */
+
+package zad1;
+
+
+public abstract class Spiewak {
+	String nazwisko;
+	int numer;
+	String tekst;
+
+	public Spiewak(String nazwisko) {
+		this.nazwisko = nazwisko;
+	}
+	abstract String spiewaj();
+	 public String toString(){
+		 return nazwisko + ": " + spiewaj();
+		 
+	 }
+	public static String najglosniej(Spiewak[] sp) {
+		
+		return null;
+	}
+}
